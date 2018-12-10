fddc6b8a9d2a5b264483023c38cecfb30f89a198
Duy Le <duyvietle13@gmail.com>
2018-12-10 09:45:51 -0800

    item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,19 @@
-const item = (function () {
+const Item = (function () {
+  var validateName = function(name){
+    if (!name){
+      throw TypeError('Name does not exist');
+    }
+  }
+    
+  var create = function (name){
     return {
-
-};
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  }
+  return {
+validateName,
+create
+  };
 }() );
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,14 @@ $(document).ready(function() {
   shoppingList.render();
 
 });
+
+const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+  } catch(error) {
+    console.log('Cannot add item: ' + error.message);
+  }
+});
+shoppingList.render();
